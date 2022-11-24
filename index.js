5cf43897c9f758e39acec0c7e051801ca82fda21
Lorraineken <lorrainekupa@gmail.com>
2022-11-24 10:35:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity ="roller-skate"){
+     if(activity=== 'bathe my dog'){
+        return `This Saturday, I want to ${activity}!`
+    }
+return`This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity ='go to the office'){
+    if(activity=== 'work from home'){
+        return `This Monday, I will ${activity}.`
+    }
+
+return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(str="*"){
+    const adjTest = function(adj="a dedicated programmer"){
+            return `You are ${str}${adj}${str}!`
+    };
+    return adjTest;
+}
